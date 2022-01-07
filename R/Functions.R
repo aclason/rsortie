3159495e35b34b17105f2d3387a4120566bbbf55
aclason <clason@unbc.ca>
2022-01-07 10:07:19 -0800

    remove helper functions

--- R/Functions.R
+++ R/Functions.R
@@ -454,3 +454,37 @@ ReadPlotFile <- function(outdir) {
   return(dt)
 
 }
+
+
+##helper function to read in .kmz files
+
+#' Read Keyhole
+#'
+#' @param file [character()] KMZ File
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' read_keyhole(file)
+#'
+read_keyhole <- function(file) {
+  # get file extension
+  ext <- strsplit(basename(file), split = '\\.')[[1]][-1]
+  # if kml
+  if (ext == 'kml') {
+    layers <- st_layers(file)$name
+    if (length(layers) > 1) {
+      return(Reduce('rbind', lapply(layers, sf::read_sf, dsn = file)))
+    }
+    return(read_sf(file))
+  } else {
+    target_file <- '.temp.kml.zip'
+    fs::file_copy(file, target_file, overwrite = T)
+    unzip(target_file, overwrite = T)
+    sf_out <- read_sf('doc.kml')
+    fs::file_delete(target_file)
+    fs::file_delete('doc.kml')
+    return(sf_out)
+  }
+}
